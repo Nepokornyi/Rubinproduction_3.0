54a8c32d6081fcba5c3fc5bc870bcc5f0b94e9c6
vadymkos <vadym.kostucok@partnersbanka.cz>
2024-02-24 20:34:20 +0100

    create FlexContainer & import icons and fonts

---
+++ rubinprod/src/components/layout/FlexContainer.tsx
@@ -0,0 +1,58 @@
+import { ReactNode } from 'react'
+import styled from 'styled-components'
+import { Direction, HorizontalAlign, VerticalAlign } from './types'
+
+type FlexContainerProps = {
+    children: ReactNode
+    direction?: Direction
+    minHeight?: string
+
+    justifyContent?: HorizontalAlign
+    alignItems?: VerticalAlign
+    center?: boolean
+
+    className?: string
+}
+
+type StyledFlexContainerProps = {
+    $direction: Direction
+    $horizontalAlign: HorizontalAlign
+    $verticalAlign: VerticalAlign
+    $minHeight: string
+}
+
+const StyledFlexContainer = styled.div<StyledFlexContainerProps>`
+    display: flex;
+    position: relative;
+    flex-direction: ${(props) => props.$direction};
+    align-items: ${(props) => props.$verticalAlign};
+    justify-content: ${(props) => props.$horizontalAlign};
+    min-height: ${(props) => props.$minHeight};
+    border: 1px solid red;
+`
+
+export const FlexContainer = ({
+    children,
+    direction = 'row',
+    minHeight = 'auto',
+    justifyContent: horizontalAlignPassed,
+    alignItems: verticalAlignPassed,
+    center = false,
+    className,
+}: FlexContainerProps) => {
+    const defaultAlign = center ? 'center' : 'none'
+    const horizontalAlign = horizontalAlignPassed ?? defaultAlign
+    const verticalAlign = verticalAlignPassed ?? defaultAlign
+
+    return (
+        <StyledFlexContainer
+            $direction={direction}
+            $horizontalAlign={horizontalAlign}
+            $verticalAlign={verticalAlign}
+            $minHeight={minHeight}
+            className={className}
+        >
+            {children}
+        </StyledFlexContainer>
+    )
+}
---
+++ rubinprod/src/components/layout/types.ts
@@ -0,0 +1,10 @@
+export type Direction = 'row' | 'column'
+export type HorizontalAlign =
+    | 'start'
+    | 'center'
+    | 'end'
+    | 'space-between'
+    | 'space-around'
+    | 'space-evenly'
+    | 'none'
+export type VerticalAlign = 'start' | 'center' | 'end' | 'none'
