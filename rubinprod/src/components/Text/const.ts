c52017d3be689c2dac8abbe47a3d0e02ddab673e
vadymkos <vadym.kostucok@partnersbanka.cz>
2024-02-24 23:59:50 +0100

    chore: correct clamp values

--- rubinprod/src/components/Text/const.ts
+++ rubinprod/src/components/Text/const.ts
@@ -8,27 +8,27 @@ export type FontFamily = 'Montserrat' | 'Open Sans' | 'Cormorant'
 
 export const variantStyles = {
     h1: css`
-        font-size: clamp(30px, 1.5vw, 120px);
+        font-size: clamp(30px, 6.5vw, 120px);
         font-weight: 700;
     `,
     h2: css`
-        font-size: clamps(30px, 1.5vw, 75px);
+        font-size: clamps(30px, 3.5vw, 75px);
         font-weight: 700;
     `,
     p: css`
-        font-size: clamp(10px, 1.5vw, 14px);
+        font-size: clamp(10px, 2.5vw, 14px);
         font-weight: 500;
     `,
     nav: css`
-        font-size: clamp(10px, 1.5vw, 18px);
+        font-size: clamp(10px, 2.5vw, 18px);
         font-weight: 400;
     `,
     socials: css`
-        font-size: clamp(9px, 1.5vw, 20px);
+        font-size: clamp(9px, 2.5vw, 20px);
         font-weight: 400;
     `,
     contacts: css`
-        font-size: clamp(12px, 1.5vw, 20px);
+        font-size: clamp(12px, 2.5vw, 20px);
         font-weight: 400;
     `,
 }
