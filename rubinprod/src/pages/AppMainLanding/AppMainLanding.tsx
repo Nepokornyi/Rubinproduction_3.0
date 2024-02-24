44862e2f734a3f4f788c4fae482400771dd9e420
vadymkos <vadym.kostucok@partnersbanka.cz>
2024-02-25 00:05:12 +0100

    chore: Create React Router

---
+++ rubinprod/src/pages/AppMainLanding/AppMainLanding.tsx
@@ -0,0 +1,17 @@
+import { MainSection } from './MainSection/MainSection'
+import { AboutSection } from './AboutSection/AboutSection'
+import { ShowReelSection } from './ShowReelSection/ShowReelSection'
+import { WorkSection } from './WorkSection/WorkSection'
+import { ContactSection } from './ContactSection/ContactSection'
+
+export const AppMainLanding = () => {
+    return (
+        <>
+            <MainSection />
+            <AboutSection />
+            <ShowReelSection />
+            <WorkSection />
+            <ContactSection />
+        </>
+    )
+}
