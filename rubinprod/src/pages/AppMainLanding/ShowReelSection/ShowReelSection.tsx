9b033e16d0b6458eb0bf22f35a843b64026864a1
vadymkos <vadym.kostucok@partnersbanka.cz>
2024-03-03 15:15:20 +0100

    feat: create showreel page

--- rubinprod/src/pages/AppMainLanding/ShowReelSection/ShowReelSection.tsx
+++ rubinprod/src/pages/AppMainLanding/ShowReelSection/ShowReelSection.tsx
@@ -1,3 +1,22 @@
+import styled from 'styled-components'
+import { FlexContainer } from '../../../components/layout/FlexContainer'
+
+import showreel from '../../../assets/img/showreel.jpg'
+
+const StyledFlexContainer = styled(FlexContainer)`
+    clip-path: polygon(0 5%, 100% 0, 100% 95%, 0 100%);
+`
+
+const StyledShowReel = styled.img`
+    width: 100%;
+    height: 100vh;
+    object-fit: cover;
+`
+
 export const ShowReelSection = () => {
-    return <div>ShowReelSection</div>
+    return (
+        <StyledFlexContainer alignItems="center" justifyContent="center">
+            <StyledShowReel src={showreel} />
+        </StyledFlexContainer>
+    )
 }
