73502b48c3d470131d393c9b58a45cb465a3cfcd
vadymkos <vadym.kostucok@partnersbanka.cz>
2024-03-05 21:25:57 +0100

    feat: create work carousel, navigation, arrows

---
+++ rubinprod/src/components/layout/Carousel/components/SwiperCustomNavigation.tsx
@@ -0,0 +1,31 @@
+import { useSwiper } from 'swiper/react'
+import { FlexContainer } from '../../FlexContainer'
+import styled from 'styled-components'
+import { SmallArrowLeft, SmallArrowRight } from '../../../Button/SmallArrow'
+
+const StyledFlexContainer = styled(FlexContainer)`
+    width: 100%;
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    z-index: 100;
+`
+
+export const SwiperCustomNavigation = () => {
+    const swiper = useSwiper()
+
+    const handlePrev = () => {
+        swiper.slidePrev()
+    }
+
+    const handleNext = () => {
+        swiper.slideNext()
+    }
+
+    return (
+        <StyledFlexContainer center>
+            <SmallArrowLeft onClick={handlePrev} />
+            <SmallArrowRight onClick={handleNext} />
+        </StyledFlexContainer>
+    )
+}
--- rubinprod/src/pages/AppMainLanding/WorkSection/WorkSection.tsx
+++ rubinprod/src/pages/AppMainLanding/WorkSection/WorkSection.tsx
@@ -1,3 +1,23 @@
+import { useTranslation } from 'react-i18next'
+import { Text } from '../../../components/Text/Text'
+import { FlexContainer } from '../../../components/layout/FlexContainer'
+import { PortfolioCarousel } from '../../../components/layout/Carousel/PortfolioCarousel'
+import styled from 'styled-components'
+
+const StyledFlexContainer = styled(FlexContainer)``
+
 export const WorkSection = () => {
-    return <div>WorkSection</div>
+    const { t } = useTranslation()
+    return (
+        <StyledFlexContainer direction="column">
+            <Text variant="h3" textTransform="uppercase">
+                <Text fontFamily="Grunges" paddingOverride="5px">
+                    our
+                </Text>
+                {t('workPage.title')}
+            </Text>
+            <Text variant="p">{t('workPage.description')}</Text>
+            <PortfolioCarousel />
+        </StyledFlexContainer>
+    )
 }
