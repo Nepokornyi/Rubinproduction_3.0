ca76fef947478a3654e0fe94e33e112f11aaa5c2
vadymkos <vadym.kostucok@partnersbanka.cz>
2024-03-10 15:10:52 +0100

    feat: create footer

---
+++ rubinprod/src/components/Footer/Footer.tsx
@@ -0,0 +1,45 @@
+import styled from 'styled-components'
+import { FlexContainer } from '../../components/layout/FlexContainer'
+import { FooterNavigation } from './FooterNavigation'
+import logo from '../../assets/img/icoRubinprodWhite.svg'
+import { Box } from '../layout/Box'
+import { Text } from '../Text/Text'
+import { useTranslation } from 'react-i18next'
+
+const StyledFlexContainer = styled(FlexContainer)`
+    clip-path: polygon(0 5%, 100% 0, 100% 95%, 0 100%);
+`
+
+const StyledBox = styled(Box)`
+    padding: 50px 25px;
+`
+
+const StyledLogo = styled.img`
+    width: 45px;
+    height: 45px;
+`
+
+export const Footer = () => {
+    const { t } = useTranslation()
+
+    return (
+        <StyledFlexContainer direction="column">
+            <FlexContainer justifyContent="space-between">
+                <StyledBox>
+                    <StyledLogo src={logo} />
+                </StyledBox>
+                <FooterNavigation />
+            </FlexContainer>
+
+            <Text
+                textAlign="center"
+                variant="p"
+                textTransform="uppercase"
+                color={'#6E6E6E'}
+                paddingOverride="0 0 15px 0"
+            >
+                {t('footer.copyright')}
+            </Text>
+        </StyledFlexContainer>
+    )
+}
---
+++ rubinprod/src/components/Footer/FooterNavigation.tsx
@@ -0,0 +1,43 @@
+import styled from 'styled-components'
+import { Text } from '../Text/Text'
+import { useTranslation } from 'react-i18next'
+
+const StyledNav = styled.nav`
+    display: flex;
+    flex-direction: column;
+    padding: 25px;
+`
+
+const StyledLink = styled.a`
+    margin: 10px 0;
+    text-decoration: none;
+    text-align: right;
+    & span:hover {
+        color: #fff;
+        transition: 0.3s;
+    }
+`
+
+export const FooterNavigation = () => {
+    const { t } = useTranslation()
+
+    return (
+        <StyledNav>
+            <StyledLink href="#">
+                <Text textTransform="uppercase" variant="p" color={'#6E6E6E'}>
+                    {t('footer.socials.behance')}
+                </Text>
+            </StyledLink>
+            <StyledLink href="#">
+                <Text textTransform="uppercase" variant="p" color={'#6E6E6E'}>
+                    {t('footer.socials.instagram')}
+                </Text>
+            </StyledLink>
+            <StyledLink href="#">
+                <Text textTransform="uppercase" variant="p" color={'#6E6E6E'}>
+                    {t('footer.socials.youtube')}
+                </Text>
+            </StyledLink>
+        </StyledNav>
+    )
+}
