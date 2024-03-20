805f4ed7a02f807e0d0c3b12b8fdf962fff2b481
vadymkos <vadym.kostucok@partnersbanka.cz>
2024-03-20 22:03:01 +0100

    experimenting with distortion effects for carousel

---
+++ rubinprod/src/components/layout/Carousel/components/PortfolioCarousel/StyledComponents.ts
@@ -0,0 +1,37 @@
+import styled from 'styled-components'
+import { FlexContainer } from '../../../FlexContainer'
+import { Swiper } from 'swiper/react'
+import { SwiperSlide } from 'swiper/react'
+
+type StyledSwiperSlideProps = {
+    $isOdd: boolean
+}
+
+export const StyledFlexContainer = styled(FlexContainer)`
+    height: 100%;
+    padding: 50px 0;
+`
+export const StyledSwiper = styled(Swiper)`
+    width: 90%;
+`
+export const StyledSwiperSlide = styled(SwiperSlide)<StyledSwiperSlideProps>`
+    height: calc(100% - 45px);
+    clip-path: ${(prop) =>
+        prop.$isOdd
+            ? 'polygon(0 5%, 100% 0, 100% 95%, 0 100%)'
+            : 'polygon(0 0, 100% 5%, 100% 100%, 0 95%)'};
+`
+export const StyledLogo = styled.img`
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    width: auto;
+    height: 50px;
+`
+export const SwiperSlideBackground = styled.img`
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    padding: 0 15px;
+`
