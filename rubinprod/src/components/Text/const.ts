f91eef1242d3bcca0b62d81f7e8b021cdf87962e
vadymkos <vadym.kostucok@partnersbanka.cz>
2024-03-23 18:52:48 +0100

    feat: refactor ui components & assets

--- rubinprod/src/components/Text/const.ts
+++ rubinprod/src/components/Text/const.ts
@@ -15,11 +15,11 @@ export type FontFamily = 'Montserrat' | 'Open Sans' | 'Grunges'
 
 export const variantStyles: Record<VariantStyles, CSSProp> = {
     h1: css`
-        font-size: clamp(40px, 8vw + 1rem, 120px);
+        font-size: clamp(40px, 7.5vw + 1rem, 120px);
         font-weight: 700;
         &::first-letter {
             opacity: 0;
-            font-size: clamp(45px, 8.5vw + 1rem, 130px);
+            font-size: clamp(45px, 8vw + 1rem, 130px);
         }
         img {
             position: absolute;
@@ -42,7 +42,7 @@ export const variantStyles: Record<VariantStyles, CSSProp> = {
         font-weight: 500;
     `,
     nav: css`
-        font-size: clamp(10px, 2.5vw, 18px);
+        font-size: clamp(14px, 2.5vw, 18px);
         font-weight: 400;
     `,
     socials: css`
@@ -51,7 +51,7 @@ export const variantStyles: Record<VariantStyles, CSSProp> = {
         text-transform: uppercase;
     `,
     button: css`
-        font-size: clamp(18px, 1.5vw, 26px);
+        font-size: clamp(18px, 1.25vw, 25px);
         font-weight: 600;
     `,
 }
