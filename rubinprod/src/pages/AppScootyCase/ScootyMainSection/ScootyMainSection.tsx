a2b10cc628ebd4a87a8c1ccacd1c59012f6ccc5d
Nepokornyi <vadymkin@gmail.com>
2024-04-27 13:49:10 +0200

    add scooty mobile landing

--- rubinprod/src/pages/AppScootyCase/ScootyMainSection/ScootyMainSection.tsx
+++ rubinprod/src/pages/AppScootyCase/ScootyMainSection/ScootyMainSection.tsx
@@ -4,13 +4,18 @@ import { LayoutFlexContainerProps } from '../../../components/layout/types'
 import { useBreakpointBiggerThan } from '../../../helpers/useCurrentBreakpoint'
 import { Header } from '../../../components/Header/Header'
 import background from '../../../assets/img/scooty/scootyLandingBackground.png'
+import backgroundSmall from '../../../assets/img/scooty/scootyLandingBackgroundSmall.png'
 import scootyLogo from '../../../assets/img/scooty/icoScootyLanding.svg'
 
-const StyledFlexContainerBackground = styled(
-    FlexContainer
-)<LayoutFlexContainerProps>`
-    background-image: url(${background});
+const StyledFlexContainerBackground = styled(FlexContainer)<
+    LayoutFlexContainerProps & { $isTabletLayout: boolean }
+>`
+    background-image: ${(props) =>
+        props.$isTabletLayout
+            ? `url(${background})`
+            : `url(${backgroundSmall})`};
     background-repeat: no-repeat;
+    background-position: center;
     background-size: cover;
     align-items: ${(props) => (props.$isDesktopLayout ? 'center' : 'initial')};
     padding: 200px 0 125px 0;
@@ -18,17 +23,23 @@ const StyledFlexContainerBackground = styled(
     clip-path: polygon(0 0, 100% 0, 100% 95%, 0 100%);
 `
 
+const StyledIcon = styled.img<{ $isDesktopLayout: boolean }>`
+    width: ${(props) => !props.$isDesktopLayout && '250px'};
+`
+
 export const ScootyMainSection = () => {
     const isDesktopLayout = useBreakpointBiggerThan('md')
+    const isTabletLayout = useBreakpointBiggerThan('sm')
 
     return (
         <StyledFlexContainerBackground
             $isDesktopLayout={isDesktopLayout}
+            $isTabletLayout={isTabletLayout}
             minHeight="100vh"
             center
         >
             <Header variants="case" />
-            <img src={scootyLogo} />
+            <StyledIcon src={scootyLogo} $isDesktopLayout={isDesktopLayout} />
         </StyledFlexContainerBackground>
     )
 }
