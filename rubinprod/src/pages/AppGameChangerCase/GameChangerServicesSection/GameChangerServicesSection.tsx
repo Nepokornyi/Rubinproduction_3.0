ca0cb45f2e4f26cc62ad2f230c528951ae00e1b2
Nepokornyi <vadymkin@gmail.com>
2024-04-30 18:45:25 +0200

    game changer services mobile

--- rubinprod/src/pages/AppGameChangerCase/GameChangerServicesSection/GameChangerServicesSection.tsx
+++ rubinprod/src/pages/AppGameChangerCase/GameChangerServicesSection/GameChangerServicesSection.tsx
@@ -6,90 +6,145 @@ import { useBreakpointBiggerThan } from '../../../helpers/useCurrentBreakpoint'
 import { Text } from '../../../components/Text/Text'
 
 import icoX from '../../../assets/img/gameChanger/icoX.png'
+import { Reveal } from '../../../components/animations/reveal/Reveal'
 
-const StyledFlexContainer = styled(FlexContainer)<LayoutFlexContainerProps>`
-    padding: 150px 0px 100px 0;
-    flex-direction: ${(props) => (props.$isDesktopLayout ? 'row' : 'column')};
+const StyledFlexContainer = styled(FlexContainer)<
+    LayoutFlexContainerProps & { $isTabletLayout: boolean }
+>`
+    padding: ${(props) =>
+        props.$isDesktopLayout ? '150px 0px 100px 0' : '75px 0px'};
+    flex-direction: ${(props) => (props.$isTabletLayout ? 'row' : 'column')};
+    align-items: ${(props) => !props.$isDesktopLayout && 'center'};
+    gap: ${(props) => !props.$isTabletLayout && '100px'};
 `
 
-const Card = styled(Box)`
-    max-width: 300px;
+const CardContainer = styled(FlexContainer)<
+    LayoutFlexContainerProps & { flexGrow: number }
+>`
+    flex: ${(props) => props.flexGrow};
+    align-items: ${(props) => !props.$isDesktopLayout && 'center'};
+    flex-direction: ${(props) => !props.$isDesktopLayout && 'column'};
+    gap: ${(props) => !props.$isDesktopLayout && '100px'};
+`
+
+const Card = styled(Box)<LayoutFlexContainerProps>`
+    width: ${(props) => (props.$isDesktopLayout ? '335px' : '250px')};
 
     ul {
         list-style: none;
         li {
-            margin-top: 10px;
+            margin-top: ${(props) => props.$isDesktopLayout && '10px'};
         }
     }
 `
 
 const TitleRelativeText = styled(Text)`
     position: relative;
+    padding: ${(props) => props.$paddingOverride};
+    text-transform: ${(props) => props.$textTransform};
 `
 
 const AbsoluteX = styled.img`
     position: absolute;
-    bottom: 0;
-    left: -55px;
+    bottom: -5px;
+    left: -35px;
 `
 
 export const GameChangerServicesSection = () => {
     const isDesktopLayout = useBreakpointBiggerThan('md')
+    const isTabletLayout = useBreakpointBiggerThan('xxl')
+
+    const upperCardJustifyContent = isTabletLayout ? 'end' : 'center'
+    const bottomCardJustifyContent = isTabletLayout ? 'start' : 'space-evenly'
 
     return (
         <StyledFlexContainer
+            $isTabletLayout={isTabletLayout}
             $isDesktopLayout={isDesktopLayout}
-            justifyContent="space-evenly"
+            gap="100px"
         >
-            <Card>
-                <TitleRelativeText
-                    $paddingOverride="0"
-                    $textTransform="uppercase"
-                    variant="button"
-                >
-                    social media advertising
-                    <AbsoluteX src={icoX} alt="" />
-                </TitleRelativeText>
-                <ul>
-                    <li>- Top funnel brand marketing video</li>
-                    <li>- Middle funnel dynamic product videos</li>
-                    <li>
-                        - Bottom funnel “application”, unboxing, “how it works”
-                        videos
-                    </li>
-                    <li>- UGC - style content</li>
-                </ul>
-            </Card>
+            <CardContainer
+                $isDesktopLayout={isDesktopLayout}
+                justifyContent={upperCardJustifyContent}
+                flexGrow={2}
+                gap="100px"
+            >
+                <Reveal removeRepeatedReveal={false}>
+                    <Card $isDesktopLayout={isDesktopLayout}>
+                        <TitleRelativeText
+                            $paddingOverride="0"
+                            $textTransform="uppercase"
+                            variant="button"
+                        >
+                            social media advertising
+                            <AbsoluteX src={icoX} alt="" />
+                        </TitleRelativeText>
+                        <ul>
+                            <li>
+                                <Text variant="p" $paddingOverride="0">
+                                    - Top funnel brand marketing video
+                                </Text>
+                            </li>
+                            <li>
+                                <Text variant="p" $paddingOverride="0">
+                                    - Middle funnel dynamic product videos
+                                </Text>
+                            </li>
+                            <li>
+                                <Text variant="p" $paddingOverride="0">
+                                    - Bottom funnel “application”, unboxing,
+                                    “how it works” videos
+                                </Text>
+                            </li>
+                            <li>
+                                <Text variant="p" $paddingOverride="0">
+                                    - UGC - style content
+                                </Text>
+                            </li>
+                        </ul>
+                    </Card>
+                </Reveal>
 
-            <Card>
-                <TitleRelativeText
-                    $paddingOverride="0"
-                    $textTransform="uppercase"
-                    variant="button"
-                >
-                    motion design / animations
-                    <AbsoluteX src={icoX} alt="" />
-                </TitleRelativeText>
-                <ul>
-                    <li>- Integrated animations</li>
-                    <li>- Separate creative video ads</li>
-                </ul>
-            </Card>
+                <Reveal removeRepeatedReveal={false}>
+                    <Card $isDesktopLayout={isDesktopLayout}>
+                        <TitleRelativeText
+                            $paddingOverride="0"
+                            $textTransform="uppercase"
+                            variant="button"
+                        >
+                            motion design / animations
+                            <AbsoluteX src={icoX} alt="" />
+                        </TitleRelativeText>
+                        <ul>
+                            <li>- Integrated animations</li>
+                            <li>- Separate creative video ads</li>
+                        </ul>
+                    </Card>
+                </Reveal>
+            </CardContainer>
 
-            <Card>
-                <TitleRelativeText
-                    $paddingOverride="0"
-                    $textTransform="uppercase"
-                    variant="button"
-                >
-                    product photography
-                    <AbsoluteX src={icoX} alt="" />
-                </TitleRelativeText>
-                <ul>
-                    <li>- For website, promoting, internal purposes</li>
-                    <li>- As thumbnails</li>
-                </ul>
-            </Card>
+            <CardContainer
+                $isDesktopLayout={isDesktopLayout}
+                justifyContent={bottomCardJustifyContent}
+                flexGrow={1}
+            >
+                <Reveal removeRepeatedReveal={false}>
+                    <Card $isDesktopLayout={isDesktopLayout}>
+                        <TitleRelativeText
+                            $paddingOverride="0"
+                            $textTransform="uppercase"
+                            variant="button"
+                        >
+                            product photography
+                            <AbsoluteX src={icoX} alt="" />
+                        </TitleRelativeText>
+                        <ul>
+                            <li>- For website, promoting, internal purposes</li>
+                            <li>- As thumbnails</li>
+                        </ul>
+                    </Card>
+                </Reveal>
+            </CardContainer>
         </StyledFlexContainer>
     )
 }
