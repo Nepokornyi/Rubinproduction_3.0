465bcc1aa0c98c287934000b764140df84b1dc47
Nepokornyi <vadymkin@gmail.com>
2024-05-04 01:19:53 +0200

    feat: elite voyage all videos

--- rubinprod/src/pages/AppEliteVoyageCase/EliteVoyageEmployeeSection/EliteVoyageEmployeeSection.tsx
+++ rubinprod/src/pages/AppEliteVoyageCase/EliteVoyageEmployeeSection/EliteVoyageEmployeeSection.tsx
@@ -1,57 +1,15 @@
-import styled from 'styled-components'
-import { FlexContainer } from '../../../components/layout/FlexContainer'
-import { Box } from '../../../components/layout/Box'
-import { Text } from '../../../components/Text/Text'
-
-import recruitmentFirst from '../../../assets/img/eliteVoyage/recruitment/recruitmentFirst.png'
-import recruitmentSecond from '../../../assets/img/eliteVoyage/recruitment/recruitmentSecond.png'
-import recruitmentThird from '../../../assets/img/eliteVoyage/recruitment/recruitmentThird.png'
-
-const StyledFlexContainer = styled(FlexContainer)`
-    padding: 150px 0px 100px 0;
-`
-
-const MarketingContainer = styled(FlexContainer)`
-    max-width: 1100px;
-`
-
-const RelativeText = styled(Text)`
-    position: relative;
-`
-
-const StyledText = styled(Text)`
-    position: absolute;
-    top: -10px;
-    right: -525px;
-    color: #b9965a;
-`
+import { useBreakpointBiggerThan } from '../../../helpers/useCurrentBreakpoint'
+import { DesktopSection } from './components/DesktopSection'
+import { MobileSection } from './components/MobileSection'
 
 export const EliteVoyageEmployeeSection = () => {
-    return (
-        <StyledFlexContainer center>
-            <MarketingContainer
-                direction="column"
-                justifyContent="center"
-                gap={'50px'}
-            >
-                <Box>
-                    <RelativeText
-                        $paddingOverride="0"
-                        $textTransform="uppercase"
-                        variant={'h2'}
-                    >
-                        Employee
-                        <StyledText fontFamily="Grunges">
-                            Spotlight Videos
-                        </StyledText>
-                    </RelativeText>
-                </Box>
-                <FlexContainer gap={'25px'}>
-                    <img src={recruitmentFirst} />
-                    <img src={recruitmentSecond} />
-                    <img src={recruitmentThird} />
-                </FlexContainer>
-            </MarketingContainer>
-        </StyledFlexContainer>
+    const isDesktopLayout = useBreakpointBiggerThan('md')
+
+    const renderContent = isDesktopLayout ? (
+        <DesktopSection />
+    ) : (
+        <MobileSection />
     )
+
+    return <>{renderContent}</>
 }
