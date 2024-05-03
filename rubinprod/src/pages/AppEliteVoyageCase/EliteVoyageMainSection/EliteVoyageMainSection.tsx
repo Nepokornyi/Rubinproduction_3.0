5a8d06f420541aa97fb2f293c2271991fb68bef8
Nepokornyi <vadymkin@gmail.com>
2024-05-03 19:48:36 +0200

    feat: elite voyage mobile landing

--- rubinprod/src/pages/AppEliteVoyageCase/EliteVoyageMainSection/EliteVoyageMainSection.tsx
+++ rubinprod/src/pages/AppEliteVoyageCase/EliteVoyageMainSection/EliteVoyageMainSection.tsx
@@ -2,34 +2,47 @@ import styled from 'styled-components'
 import { FlexContainer } from '../../../components/layout/FlexContainer'
 import { LayoutFlexContainerProps } from '../../../components/layout/types'
 import { useBreakpointBiggerThan } from '../../../helpers/useCurrentBreakpoint'
-import { Header } from '../../../components/Header/Header'
+import { Reveal } from '../../../components/animations/reveal/Reveal'
 
-import background from '../../../assets/img/eliteVoyage/eliteVoyageLandingBackground.png'
-import eliteVoyageLogo from '../../../assets/img/eliteVoyage/icoEliteVoyageLog.png'
+import background from '../../../assets/img/eliteVoyage/landingBackground.png'
+import backgroundSmall from '../../../assets/img/eliteVoyage/landingBackgroundSmall.png'
+import eliteVoyageLogo from '../../../assets/img/eliteVoyage/icoEliteVoyage.svg'
 
-const StyledFlexContainerBackground = styled(
-    FlexContainer
-)<LayoutFlexContainerProps>`
-    background-image: url(${background});
+const StyledFlexContainerBackground = styled(FlexContainer)<
+    LayoutFlexContainerProps & { $isTabletLayout: boolean }
+>`
+    background-image: ${(props) =>
+        props.$isTabletLayout
+            ? `url(${background})`
+            : `url(${backgroundSmall})`};
     background-repeat: no-repeat;
     background-size: cover;
-    align-items: ${(props) => (props.$isDesktopLayout ? 'center' : 'initial')};
     padding: 200px 0 125px 0;
     overflow: hidden;
     clip-path: polygon(0 0, 100% 0, 100% 95%, 0 100%);
 `
 
+const StyledIcon = styled.img<{ $isDesktopLayout: boolean }>`
+    width: ${(props) => (props.$isDesktopLayout ? '300px' : '250px')};
+`
+
 export const EliteVoyageMainSection = () => {
     const isDesktopLayout = useBreakpointBiggerThan('md')
+    const isTabletLayout = useBreakpointBiggerThan('sm')
 
     return (
         <StyledFlexContainerBackground
             $isDesktopLayout={isDesktopLayout}
+            $isTabletLayout={isTabletLayout}
             minHeight="100vh"
             center
         >
-            <Header variants="case" />
-            <img src={eliteVoyageLogo} />
+            <Reveal delay={1.5}>
+                <StyledIcon
+                    $isDesktopLayout={isDesktopLayout}
+                    src={eliteVoyageLogo}
+                />
+            </Reveal>
         </StyledFlexContainerBackground>
     )
 }
