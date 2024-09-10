59caab438c1034be5da674487d8fd138a70cca22
Nepokornyi <vadymkin@gmail.com>
2024-09-10 19:47:09 +0200

    feat: setup next-intl

--- nextjs_rubinprod/rubinprod/src/app/[locale]/layout.tsx
+++ nextjs_rubinprod/rubinprod/src/app/[locale]/layout.tsx
@@ -1,6 +1,8 @@
+import { NextIntlClientProvider } from 'next-intl'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import './globals.css'
+import '../globals.css'
+import { getMessages } from 'next-intl/server'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -10,14 +12,22 @@ export const metadata: Metadata = {
         'Discover our creative video making atelier, specializing in high-quality video production, editing, and storytelling. Elevate your brand with professional visuals tailored to your vision.',
 }
 
-export default function RootLayout({
+export default async function LocaleLayout({
     children,
+    params,
 }: Readonly<{
     children: React.ReactNode
+    params: { locale: string }
 }>) {
+    const messages = await getMessages()
+
     return (
-        <html lang="en">
-            <body className={inter.className}>{children}</body>
+        <html lang={params.locale}>
+            <body className={inter.className}>
+                <NextIntlClientProvider messages={messages}>
+                    {children}
+                </NextIntlClientProvider>
+            </body>
         </html>
     )
 }
