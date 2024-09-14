e2f64826b88706eb7a359d9b8001d73c96847725
Nepokornyi <vadymkin@gmail.com>
2024-09-14 14:44:36 +0200

    feat: nextjs Input component

---
+++ nextjs_rubinprod/rubinprod/src/components/Form/types.ts
@@ -0,0 +1,8 @@
+export const InputVariants = {
+    default: 'focus:border-[#d91e37]',
+    scooty: 'focus:border-[#D7F000]',
+    gameChanger: 'focus:border-[#7055EC]',
+    eliteVoyage: 'focus:border-[#B9965A]',
+}
+
+export type InputVariantsList = keyof typeof InputVariants
