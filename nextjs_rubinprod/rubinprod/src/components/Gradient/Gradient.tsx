0cab7cf081ae983229797166e36757655256e993
Nepokornyi <vadymkin@gmail.com>
2024-09-20 22:28:35 +0200

    feat: tailwind clip-path refactor

--- nextjs_rubinprod/rubinprod/src/components/Gradient/Gradient.tsx
+++ nextjs_rubinprod/rubinprod/src/components/Gradient/Gradient.tsx
@@ -7,7 +7,7 @@ type GradientProps = {
 
 export const Gradient = ({ children }: GradientProps) => {
     return (
-        <Box className="bg-gradient-to-b from-[#0C0C0C] to-transparent md:bg-gradient-to-b md:from-[#0C0C0C] md:to-[#BF1B32] clip-custom-path">
+        <Box className="bg-gradient-to-b from-[#0C0C0C] to-transparent md:bg-gradient-to-b md:from-[#0C0C0C] md:to-[#BF1B32] clip-path-gradient">
             {children}
         </Box>
     )
