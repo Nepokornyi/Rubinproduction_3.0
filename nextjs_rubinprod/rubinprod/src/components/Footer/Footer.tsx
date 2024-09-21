0bedef4cf2bc270e7565e4adbc76231496cdce1e
Nepokornyi <vadymkin@gmail.com>
2024-09-21 12:54:55 +0200

    feat: nextjs basic footer

---
+++ nextjs_rubinprod/rubinprod/src/components/Footer/Footer.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { FlexContainer } from '../FlexContainer/FlexContainer'
+import { Text } from '../Text/Text'
+import { Box } from '../Box/Box'
+
+export const Footer = () => {
+    return (
+        <FlexContainer direction="flex-col" center>
+            <FlexContainer
+                justifyContent="justify-between"
+                alignItems="items-end"
+            >
+                <div className="pb-6">Logo</div>
+
+                <Text variant="socials" padding="pb-6">
+                    Copyright
+                </Text>
+
+                <Box className="grid grid-cols-3 grid-rows-3 pb-6 gap-x-4 text-right">
+                    <a href="" className="col-start-3 row-start-1">
+                        Home
+                    </a>
+                    <a href="" className="col-start-1 row-start-2">
+                        About
+                    </a>
+                    <a href="" className="col-start-2 row-start-2">
+                        Contact
+                    </a>
+                    <a href="" className="col-start-3 row-start-2">
+                        Portfolio
+                    </a>
+                    <a href="" className="col-start-1 row-start-3">
+                        Behance
+                    </a>
+                    <a href="" className="col-start-2 row-start-3">
+                        Instagram
+                    </a>
+                    <a href="" className="col-start-3 row-start-3">
+                        Youtube
+                    </a>
+                </Box>
+            </FlexContainer>
+        </FlexContainer>
+    )
+}
