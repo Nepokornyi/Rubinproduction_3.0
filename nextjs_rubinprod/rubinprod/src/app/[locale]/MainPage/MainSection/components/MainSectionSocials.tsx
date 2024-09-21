712ed0618722cb0deb0082d3c56425172c419f1e
Nepokornyi <vadymkin@gmail.com>
2024-09-21 16:47:40 +0200

    feat: main section text styling

--- nextjs_rubinprod/rubinprod/src/app/[locale]/MainPage/MainSection/components/MainSectionSocials.tsx
+++ nextjs_rubinprod/rubinprod/src/app/[locale]/MainPage/MainSection/components/MainSectionSocials.tsx
@@ -1,11 +1,38 @@
+import { FlexContainer } from '@/components/FlexContainer/FlexContainer'
+import { Text } from '@/components/Text/Text'
 import React from 'react'
 
 export const MainSectionSocials = () => {
     return (
-        <div className=" absolute -bottom-12 md:bottom-auto md:-left-32 md:top-1/2 md:-rotate-90 flex gap-1">
-            <div>Socials</div>
-            <div>Socials</div>
-            <div>Socials</div>
-        </div>
+        <FlexContainer
+            gap="gap-10"
+            alignItems="items-center"
+            className="absolute bottom-0 md:bottom-auto md:[writing-mode:vertical-lr] md:rotate-180 md:w-auto"
+        >
+            <a href="https://www.instagram.com/who1snick">
+                <Text
+                    variant="socials"
+                    className="text-[#6e6e6e] hover:text-white transition-colors"
+                >
+                    Instagram
+                </Text>
+            </a>
+            <a href="https://www.behance.net/who1snick">
+                <Text
+                    variant="socials"
+                    className="text-[#6e6e6e] hover:text-white transition-colors"
+                >
+                    Behance
+                </Text>
+            </a>
+            <a href="https://www.youtube.com/@who1snick466/videos">
+                <Text
+                    variant="socials"
+                    className="text-[#6e6e6e] hover:text-white transition-colors"
+                >
+                    Youtube
+                </Text>
+            </a>
+        </FlexContainer>
     )
 }
--- nextjs_rubinprod/rubinprod/src/components/FlexContainer/types.ts
+++ nextjs_rubinprod/rubinprod/src/components/FlexContainer/types.ts
@@ -9,7 +9,15 @@ export type HorizontalAlign =
     | ''
 export type VerticalAlign = 'items-start' | 'items-center' | 'items-end' | ''
 
-export type Gap = 'gap-0' | 'gap-1' | 'gap-2' | 'gap-4' | 'gap-6' | 'gap-8'
+export type Gap =
+    | 'gap-0'
+    | 'gap-1'
+    | 'gap-2'
+    | 'gap-4'
+    | 'gap-6'
+    | 'gap-8'
+    | 'gap-9'
+    | 'gap-10'
 
 export type MinHeight =
     | 'min-h-100vh'
