341467f5dc200e6a4f18fc11a8b217f391044d15
Nepokornyi <vadymkin@gmail.com>
2024-09-21 11:44:28 +0200

    feat: nextjs basic form section

---
+++ nextjs_rubinprod/rubinprod/src/app/[locale]/MainPage/ContactSection/components/ContactSectionForm.tsx
@@ -0,0 +1,19 @@
+import { Button } from '@/components/Button/Button'
+import { FlexContainer } from '@/components/FlexContainer/FlexContainer'
+import Input from '@/components/Form/Input'
+import React from 'react'
+
+export const ContactSectionForm = () => {
+    return (
+        <form>
+            <FlexContainer direction="flex-col">
+                <Input placeholder="email" />
+                <Input placeholder="phone" />
+                <Button className="md:border-none md:bg-black">
+                    {' '}
+                    Poslat Zadost
+                </Button>
+            </FlexContainer>
+        </form>
+    )
+}
