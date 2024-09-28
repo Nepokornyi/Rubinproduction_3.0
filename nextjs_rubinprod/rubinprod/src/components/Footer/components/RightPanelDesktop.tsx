bf8b94e3fcb3918f598342c377744f59561cd100
Nepokornyi <vadymkin@gmail.com>
2024-09-29 01:28:53 +0200

    feat: nextjs footer

---
+++ nextjs_rubinprod/rubinprod/src/components/Footer/components/RightPanelDesktop.tsx
@@ -0,0 +1,62 @@
+import { Text } from '@/components/Text/Text'
+import { Link } from '@/navigation'
+import { useTranslations } from 'next-intl'
+import React from 'react'
+
+export const RightPanelDesktop = () => {
+    const t = useTranslations('Footer')
+
+    const LinkList = [
+        {
+            href: '/',
+            text: t('navigation.home'),
+            className: 'col-start-3 row-start-1',
+        },
+        {
+            href: '#about',
+            text: t('navigation.about'),
+            className: 'col-start-1 row-start-2',
+        },
+        {
+            href: '#contact',
+            text: t('navigation.contact'),
+            className: 'col-start-2 row-start-2',
+        },
+        {
+            href: '#portfolio',
+            text: t('navigation.portfolio'),
+            className: 'col-start-3 row-start-2',
+        },
+        {
+            href: 'https://www.behance.net/who1snick',
+            text: t('socials.behance'),
+            className: 'col-start-1 row-start-3',
+        },
+        {
+            href: 'https://www.instagram.com/who1snick',
+            text: t('socials.instagram'),
+            className: 'col-start-2 row-start-3',
+        },
+        {
+            href: 'https://www.youtube.com/@who1snick466/videos',
+            text: t('socials.youtube'),
+            className: 'col-start-3 row-start-3',
+        },
+    ]
+
+    return (
+        <>
+            {LinkList.map((link, index) => (
+                <Link key={index} href={link.href} className={link.className}>
+                    <Text
+                        textTransform="uppercase"
+                        className="text-[#6e6e6e] hover:text-white transition-colors"
+                        padding="px-6"
+                    >
+                        {link.text}
+                    </Text>
+                </Link>
+            ))}
+        </>
+    )
+}
