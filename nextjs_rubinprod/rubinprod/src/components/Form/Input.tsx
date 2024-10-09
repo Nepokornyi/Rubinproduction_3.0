4d318619cf0ef842ef671b4b4078364248b1af0c
Nepokornyi <vadymkin@gmail.com>
2024-10-09 21:54:38 +0200

    feat: update Input styles & add Errors

--- nextjs_rubinprod/rubinprod/src/components/Form/Input.tsx
+++ nextjs_rubinprod/rubinprod/src/components/Form/Input.tsx
@@ -1,23 +1,47 @@
 import { forwardRef, InputHTMLAttributes } from 'react'
 import { Text } from '../Text/Text'
-import { InputVariants, InputVariantsList } from './types'
+import {
+    ErrorVariants,
+    ErrorVariantsList,
+    InputVariants,
+    InputVariantsList,
+} from './types'
+import { FlexContainer } from '../FlexContainer/FlexContainer'
 
 type InputProps = InputHTMLAttributes<HTMLInputElement> & {
     error?: string
     color?: InputVariantsList
+    errorColor?: ErrorVariantsList
 }
 
 const Input = forwardRef<HTMLInputElement, InputProps>(
-    ({ error, color = 'default', className, ...props }, ref) => {
+    (
+        {
+            error,
+            color = 'default',
+            errorColor = 'default',
+            className,
+            ...props
+        },
+        ref
+    ) => {
         return (
-            <>
+            <FlexContainer direction="flex-col" gap={'gap-1'} className="my-4">
                 <input
-                    className={`bg-transparent pb-2.5 my-4 border-b-2 border-white text-lg focus:outline-none focus:border-b-2 focus:ring-0 placeholder:uppercase placeholder-opacity-30 placeholder:font-semibold ${InputVariants[color]} ${className} transition-colors`}
+                    className={`bg-transparent pb-2.5 border-b-2 border-white text-lg focus:outline-none focus:border-b-2 focus:ring-0 placeholder:uppercase placeholder-opacity-30 placeholder:font-semibold ${InputVariants[color]} ${className} transition-colors`}
                     ref={ref}
                     {...props}
                 />
-                {error && <Text>{error}</Text>}
-            </>
+                {error && (
+                    <Text
+                        fontWeight="font-semibold"
+                        padding="px-0"
+                        className={ErrorVariants[errorColor]}
+                    >
+                        {error}
+                    </Text>
+                )}
+            </FlexContainer>
         )
     }
 )
