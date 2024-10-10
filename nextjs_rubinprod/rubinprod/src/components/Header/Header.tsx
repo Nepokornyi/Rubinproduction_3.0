73091aa9fa5bc32f90ba56ac24b57dabe03899fd
Nepokornyi <vadymkin@gmail.com>
2024-10-10 11:25:19 +0200

    main landing final adjustments

--- nextjs_rubinprod/rubinprod/src/components/Header/Header.tsx
+++ nextjs_rubinprod/rubinprod/src/components/Header/Header.tsx
@@ -6,6 +6,9 @@ import logo from '@/assets/img/RubinLetter.svg'
 import { DesktopMenu } from './components/DesktopMenu'
 import { MobileMenu } from './components/MobileMenu/MobileMenu'
 import { useScrollHeader } from '@/hooks/useScrollHeader'
+import { Link } from '@/navigation'
+import { motion } from 'framer-motion'
+import animation from './components/config/configLogo'
 
 export const Header = () => {
     const headerStyle = useScrollHeader()
@@ -15,11 +18,19 @@ export const Header = () => {
         <header
             className={`flex justify-between items-center w-full md:w-3/4 fixed md:absolute top-0 left-1/2 transform -translate-x-1/2 p-4 z-50   ${headerStyle} bg-[#0c0c0c] md:bg-transparent transition-colors`}
         >
-            <Image
-                src={logo}
-                alt="Rubinproduction Logo"
-                className="w-12 md:w-24"
-            />
+            <motion.div
+                variants={animation.container}
+                initial="initial"
+                animate="animate"
+            >
+                <Link href="/">
+                    <Image
+                        src={logo}
+                        alt="Rubinproduction Logo"
+                        className="w-12 md:w-24"
+                    />
+                </Link>
+            </motion.div>
             <DesktopMenu />
             <MobileMenu />
         </header>
