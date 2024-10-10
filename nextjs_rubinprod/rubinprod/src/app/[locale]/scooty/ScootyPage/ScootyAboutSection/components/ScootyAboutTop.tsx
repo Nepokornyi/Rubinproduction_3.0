34d182ee9caea81bce1ce38583e5bb65d4a2bed1
Nepokornyi <vadymkin@gmail.com>
2024-10-10 23:21:07 +0200

    feat: nextjs Scooty About Section

---
+++ nextjs_rubinprod/rubinprod/src/app/[locale]/scooty/ScootyPage/ScootyAboutSection/components/ScootyAboutTop.tsx
@@ -0,0 +1,44 @@
+import { FlexContainer } from '@/components/FlexContainer/FlexContainer'
+import { Text } from '@/components/Text/Text'
+import Image from 'next/image'
+import React from 'react'
+
+import logo from '@/assets/img/scooty/ScootyLogoGreen.svg'
+
+export const ScootyAboutTop = () => {
+    return (
+        <>
+            <div className="row-start-2 md:row-start-1 col-span-2 md:col-span-1 place-self-center md:place-self-auto">
+                <Image
+                    src={logo}
+                    alt="scooty logo"
+                    className="w-48 md:w-auto mb-20"
+                />
+            </div>
+            <FlexContainer direction="flex-col">
+                <Text variant="nav" className="mb-4 md:mb-8">
+                    Client
+                </Text>
+                <Text
+                    variant="button"
+                    textTransform="uppercase"
+                    className="mb-20"
+                >
+                    Scooty
+                </Text>
+            </FlexContainer>
+            <FlexContainer direction="flex-col">
+                <Text variant="nav" className="mb-4 md:mb-8">
+                    Industry
+                </Text>
+                <Text
+                    variant="button"
+                    textTransform="uppercase"
+                    className="mb-20"
+                >
+                    Food Delivery
+                </Text>
+            </FlexContainer>
+        </>
+    )
+}
