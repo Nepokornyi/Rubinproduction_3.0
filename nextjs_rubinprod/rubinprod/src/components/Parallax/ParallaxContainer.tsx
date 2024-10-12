094b2d35eddc4db071d30ada441943050532a570
Nepokornyi <vadymkin@gmail.com>
2024-10-13 00:12:08 +0200

    feat: nextjs extend parallax text component

--- nextjs_rubinprod/rubinprod/src/components/Parallax/ParallaxContainer.tsx
+++ nextjs_rubinprod/rubinprod/src/components/Parallax/ParallaxContainer.tsx
@@ -5,21 +5,27 @@ import { ParallaxText } from './components/ParallaxText'
 type ParallaxTextProps = {
     text: string
     isTransparent?: boolean
+    isCase?: boolean
+    placeBottom?: boolean
 }
 
 export const ParallaxContainer = ({
     text,
     isTransparent,
+    isCase = false,
+    placeBottom = false,
 }: ParallaxTextProps) => {
     const background = isTransparent
         ? 'bg-transparent'
         : 'bg-gradient-to-r from-[#0C0C0C] to-[#BF1B32]'
 
+    const position = placeBottom ? '-bottom-4 lg:-bottom-6' : '-top-4 lg:-top-6'
+
     return (
         <FlexContainer
-            className={`absolute -top-4 lg:-top-6 py-4 md:py-6 -skew-y-3 md:-skew-y-2 lg:-skew-y-1 ${background}`}
+            className={`absolute ${position} py-4 md:py-6 -skew-y-3 md:-skew-y-2 lg:-skew-y-1 ${background}`}
         >
-            <ParallaxText>{text}</ParallaxText>
+            <ParallaxText isCase={isCase}>{text}</ParallaxText>
         </FlexContainer>
     )
 }
