9159f79c726268fcf34316016aca551ce3a2fba2
Nepokornyi <vadymkin@gmail.com>
2024-10-14 00:19:04 +0200

    feat: scooty about section arrow animation

--- nextjs_rubinprod/rubinprod/src/app/[locale]/scooty/ScootyPage/ScootyAboutSection/ScootyAboutSection.tsx
+++ nextjs_rubinprod/rubinprod/src/app/[locale]/scooty/ScootyPage/ScootyAboutSection/ScootyAboutSection.tsx
@@ -8,7 +8,7 @@ import { ScootyAboutBottom } from './components/ScootyAboutBottom'
 export const ScootyAboutSection = () => {
     return (
         <FlexContainer id="about" center>
-            <Box className="grid md:w-11/12 xl:w-3/4 2xl:w-3/5 grid-rows-[auto,1fr]  grid-cols-2 md:grid-cols-3 gap-x-14 pt-36 pb-24">
+            <Box className="grid md:w-11/12 xl:w-3/4 2xl:w-3/5 grid-rows-[auto,1fr]  grid-cols-2 md:grid-cols-3 gap-x-14 pt-36 pb-24 px-6">
                 <ScootyAboutTop />
                 <ScootyAboutBottom />
             </Box>
--- nextjs_rubinprod/rubinprod/src/app/[locale]/scooty/ScootyPage/ScootyAboutSection/components/ScootyAboutBottom.tsx
+++ nextjs_rubinprod/rubinprod/src/app/[locale]/scooty/ScootyPage/ScootyAboutSection/components/ScootyAboutBottom.tsx
@@ -1,6 +1,7 @@
 import { Arrow } from '@/components/Arrow/Arrow'
 import { Box } from '@/components/Box/Box'
 import { FlexContainer } from '@/components/FlexContainer/FlexContainer'
+import { Reveal } from '@/components/Reveal/Reveal'
 import { Text } from '@/components/Text/Text'
 import { useTranslations } from 'next-intl'
 import React from 'react'
@@ -59,8 +60,14 @@ export const ScootyAboutBottom = () => {
                 direction="flex-col"
                 className="relative col-span-2 md:col-span-1 px-6 md:px-0 mb-20"
             >
-                <div className="absolute md:hidden h-full bottom-0 -left-0 px-6 md:py-2.5">
-                    <Arrow direction="down" isFullHeight />
+                <div className="absolute md:hidden h-full bottom-0 -left-0 px-6">
+                    <Reveal
+                        y={-35}
+                        removeRepeatedReveal={false}
+                        className="h-full"
+                    >
+                        <Arrow direction="down" isFullHeight />
+                    </Reveal>
                 </div>
 
                 <Text variant="nav" className="mb-4 md:mb-6">
