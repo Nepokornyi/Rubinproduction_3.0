1942beee5081da431b75ce97943c83925c66b1b8
Nepokornyi <vadymkin@gmail.com>
2024-10-14 00:10:57 +0200

    fix: refactor to proper links

--- nextjs_rubinprod/rubinprod/src/app/[locale]/scooty/ScootyPage/ScootyAboutSection/ScootyAboutSection.tsx
+++ nextjs_rubinprod/rubinprod/src/app/[locale]/scooty/ScootyPage/ScootyAboutSection/ScootyAboutSection.tsx
@@ -7,7 +7,7 @@ import { ScootyAboutBottom } from './components/ScootyAboutBottom'
 
 export const ScootyAboutSection = () => {
     return (
-        <FlexContainer center>
+        <FlexContainer id="about" center>
             <Box className="grid md:w-11/12 xl:w-3/4 2xl:w-3/5 grid-rows-[auto,1fr]  grid-cols-2 md:grid-cols-3 gap-x-14 pt-36 pb-24">
                 <ScootyAboutTop />
                 <ScootyAboutBottom />
