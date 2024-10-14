d5295d83c0c33ccef4af232d0ea6644988179947
Nepokornyi <vadymkin@gmail.com>
2024-10-15 00:22:21 +0200

    feat: nextjs GameChanger Showreel Section

--- nextjs_rubinprod/rubinprod/src/app/[locale]/gamechanger/page.tsx
+++ nextjs_rubinprod/rubinprod/src/app/[locale]/gamechanger/page.tsx
@@ -3,6 +3,7 @@ import { Header } from '@/components/Header/Header'
 import { ContactSection } from '../components/ContactSection/ContactSection'
 import { GameChangerMainSection } from './GameChangerPage/GameChangerMainSection/GameChangerMainSection'
 import { GameChangerAboutSection } from './GameChangerPage/GameChangerAboutSection/GameChangerAboutSection'
+import { GameChangerShowReelSection } from './GameChangerPage/GameChangerShowReelSection/GameChangerShowReelSection'
 
 export default function GameChanger() {
     return (
@@ -10,7 +11,7 @@ export default function GameChanger() {
             <Header isCase />
             <GameChangerMainSection />
             <GameChangerAboutSection />
-            <div>GameChangerShowReelSection</div>
+            <GameChangerShowReelSection />
             <div>GameChangerServicesSection</div>
             <div>Marketing</div>
             <div>GameChangerShowReelSection</div>
