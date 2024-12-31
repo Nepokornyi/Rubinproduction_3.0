79fbafb033f675cb28e0a24934564763967042f4
Nepokornyi <vadymkin@gmail.com>
2024-12-31 01:16:07 +0100

    feat: better error handling

--- nextjs_rubinprod/src/app/[locale]/login/LoginPage/LoginForm/const.ts
+++ nextjs_rubinprod/src/app/[locale]/login/LoginPage/LoginForm/const.ts
@@ -3,6 +3,6 @@ export type LoginStatus = 'idle' | 'loading' | 'success' | 'error'
 export const LoginTitleVariants = {
     idle: '',
     loading: '',
-    success: 'Success',
-    error: 'Error',
+    success: 'Vítej',
+    error: 'Chyba',
 }
