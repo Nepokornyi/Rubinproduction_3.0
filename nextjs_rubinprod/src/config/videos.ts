21974b46bd8089c6cdc2d879e9f7ee1b51d49e20
Nepokornyi <vadymkin@gmail.com>
2025-01-12 19:37:10 +0100

    feat: add new video

--- nextjs_rubinprod/src/config/videos.ts
+++ nextjs_rubinprod/src/config/videos.ts
@@ -1,6 +1,14 @@
 export const videoConfig = [
     {
         id: 1,
+        title: 'Telefon nebo Kamera: Jak vybrat správný nástroj pro tvorbu obsahu?',
+        videoUrl: 1045037548,
+        preview: 1045037548,
+        description:
+            'Igor Glinka, student filmové školy ze zkušenostmi vedení produkčních týmů, shrnuje výhody a nevýhody natáčení na telefon i kameru. Zjistíte, který nástroj se hodí pro vaše potřeby, a získáte praktické tipy pro tvorbu obsahu.',
+    },
+    {
+        id: 2,
         title: 'Tomáš Nygrýn - Creative Director | ShareYourself Agency',
         videoUrl: 1042300569,
         preview: 1042916606,
@@ -8,7 +16,7 @@ export const videoConfig = [
             'Zjistěte, jak vypadá běžný den Creative Directora při spolupráci s klientem. Tomáš Nygrýn sdílí své know-how, tipy na efektivní workflow a náhled do kreativního procesu.',
     },
     {
-        id: 2,
+        id: 3,
         title: 'Mark Bliss ACS - Jak se dostat do Kinematografie?',
         videoUrl: 1042304330,
         preview: 1042919596,
@@ -16,7 +24,7 @@ export const videoConfig = [
             'Rozhovor s držitelem prestižních ocenění a vítězem Cannes Festivalu! Mark Bliss ACS sdílí svůj příběh o cestě do světa kinematografie, inspiraci a zákulisí úspěchu. Dozvíte se, co stojí za jeho kariérou a jaké rady má pro začínající i pokročilé tvůrce',
     },
     {
-        id: 3,
+        id: 4,
         title: 'Generative Fill ve videu? Photoshop + PremirePro | Tutorial',
         videoUrl: 1042303511,
         preview: 1042925578,
