2b481795bcc808fb3fe4ce207434f77a3831f31a
Nepokornyi <vadymkin@gmail.com>
2025-01-28 23:52:15 +0100

    feat: add lock on paid videos

--- nextjs_rubinprod/src/app/[locale]/community/CommunityPage/CommunityVideos/CommunityVideosGrid.tsx
+++ nextjs_rubinprod/src/app/[locale]/community/CommunityPage/CommunityVideos/CommunityVideosGrid.tsx
@@ -1,11 +1,13 @@
 import { Box } from '@/components/Box/Box'
 import React from 'react'
 import { FlexContainer } from '@/components/FlexContainer/FlexContainer'
+import { useRouter } from 'next/navigation'
 import { Text } from '@/components/Text/Text'
+import { subscribe } from '../../utils/subscriptionApi'
+import { useSubscriptionContext } from '../../context/SubscriptionContext'
 import { VimeoVideo } from '../../../components/VimeoVideo/VimeoVideo'
 import { communityGridStyles, CommunityVideosGridTypes } from './const'
 import { FaLock } from 'react-icons/fa'
-import { useSubscriptionContext } from '../../context/SubscriptionContext'
 
 type CommunityVideosGridProps = {
     content: any[]
@@ -17,6 +19,22 @@ export const CommunityVideosGrid = ({
     variant,
 }: CommunityVideosGridProps) => {
     const { isSubscribed } = useSubscriptionContext()
+    const router = useRouter()
+
+    const handleSubscribe = async () => {
+        const locale = window.location.pathname.split('/')[1]
+
+        try {
+            const { url } = await subscribe(locale)
+            router.push(url)
+        } catch (error) {}
+    }
+
+    const handleLockClick = (e: React.MouseEvent<SVGElement>) => {
+        const lockIcon = e.currentTarget
+        lockIcon.classList.add('animate-pulse')
+        setTimeout(handleSubscribe, 300)
+    }
 
     return (
         <Box className="w-full grid grid-cols-1 px-5 gap-5 lg:grid-cols-2 auto-rows-auto">
@@ -36,7 +54,10 @@ export const CommunityVideosGrid = ({
                                 {title}
                             </Text>
                             {!isSubscribed && variant === 'paid' && (
-                                <FaLock className="mt-1.5" />
+                                <FaLock
+                                    className="mt-1.5 cursor-pointer transition-transform"
+                                    onClick={handleLockClick}
+                                />
                             )}
                         </FlexContainer>
                         <Text
