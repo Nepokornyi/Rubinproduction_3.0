8b9d80d8ac5f2c9fe0e4d1fe88fc8dcb967f2c96
Nepokornyi <vadymkin@gmail.com>
2025-02-01 17:06:34 +0100

    update free & paid videos

--- nextjs_rubinprod/src/config/freeVideos.ts
+++ nextjs_rubinprod/src/config/freeVideos.ts
@@ -1,6 +1,22 @@
 export const freeVideoConfig = [
     {
         id: 1,
+        title: 'Mark Bliss ACS - Jak se dostat do Kinematografie?',
+        videoUrl: 1042304330,
+        preview: 1042919596,
+        description:
+            'Rozhovor s držitelem prestižních ocenění a vítězem Cannes Festivalu! Mark Bliss ACS sdílí svůj příběh o cestě do světa kinematografie, inspiraci a zákulisí úspěchu. Dozvíte se, co stojí za jeho kariérou a jaké rady má pro začínající i pokročilé tvůrce',
+    },
+    {
+        id: 2,
+        title: 'Tomáš Nygrýn - Creative Director | ShareYourself Agency',
+        videoUrl: 1042300569,
+        preview: 1042916606,
+        description:
+            'Zjistěte, jak vypadá běžný den Creative Directora při spolupráci s klientem. Tomáš Nygrýn sdílí své know-how, tipy na efektivní workflow a náhled do kreativního procesu.',
+    },
+    {
+        id: 3,
         title: 'Profese Video Editora za 2 hodiny',
         videoUrl: 1042286031,
         preview: 1042286031,
@@ -8,11 +24,19 @@ export const freeVideoConfig = [
             'Toto video vás provede klíčovými funkcemi Adobe Premiere Pro 2025 – od organizace projektu a importu záběrů až po finální export. Žádná omáčka, jenom praktické znalosti z reálného světa střihu videí.',
     },
     {
-        id: 2,
+        id: 4,
         title: 'Telefon nebo Kamera: Jak vybrat správný nástroj pro tvorbu obsahu?',
         videoUrl: 1045037548,
         preview: 1045037548,
         description:
             'Igor Glinka, student filmové školy ze zkušenostmi vedení produkčních týmů, shrnuje výhody a nevýhody natáčení na telefon i kameru. Zjistíte, který nástroj se hodí pro vaše potřeby, a získáte praktické tipy pro tvorbu obsahu.',
+    },
+    {
+        id: 5,
+        title: 'Produkční den sportovního spotu | Tenis',
+        videoUrl: 1051244389,
+        preview: 1051244389,
+        description:
+            'Zorganizovat natáčení reklamního spotu není jednoduché - nápad, lokace, tým, technika, modelky, struktura a postprodukce. Ale občas není potřeba mít velké budgety, zvlášť v případě, když děláte projekt do vlastního portfolia.',
     },
 ]
--- nextjs_rubinprod/src/config/paidVideos.ts
+++ nextjs_rubinprod/src/config/paidVideos.ts
@@ -1,21 +1,5 @@
 export const paidVideoConfig = [
     {
-        id: 1,
-        title: 'Tomáš Nygrýn - Creative Director | ShareYourself Agency',
-        videoUrl: 1042300569,
-        preview: 1042916606,
-        description:
-            'Zjistěte, jak vypadá běžný den Creative Directora při spolupráci s klientem. Tomáš Nygrýn sdílí své know-how, tipy na efektivní workflow a náhled do kreativního procesu.',
-    },
-    {
-        id: 2,
-        title: 'Mark Bliss ACS - Jak se dostat do Kinematografie?',
-        videoUrl: 1042304330,
-        preview: 1042919596,
-        description:
-            'Rozhovor s držitelem prestižních ocenění a vítězem Cannes Festivalu! Mark Bliss ACS sdílí svůj příběh o cestě do světa kinematografie, inspiraci a zákulisí úspěchu. Dozvíte se, co stojí za jeho kariérou a jaké rady má pro začínající i pokročilé tvůrce',
-    },
-    {
         id: 3,
         title: 'Generative Fill ve videu? Photoshop + PremirePro | Tutorial',
         videoUrl: 1042303511,
