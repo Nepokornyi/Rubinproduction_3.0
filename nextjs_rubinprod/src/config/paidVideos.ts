30db7f49e0c997b7cb472fdf4a796302604e20b7
Nepokornyi <vadymkin@gmail.com>
2025-02-06 15:59:03 +0100

    add new video

--- nextjs_rubinprod/src/config/paidVideos.ts
+++ nextjs_rubinprod/src/config/paidVideos.ts
@@ -1,10 +1,18 @@
 export const paidVideoConfig = [
     {
-        id: 3,
+        id: 1,
         title: 'Generative Fill ve videu? Photoshop + PremirePro | Tutorial',
         videoUrl: 1042303511,
-        preview: 1042925578,
+        preview: 1042303511,
         description:
             'V tomto tutoriálu vám ukážeme, jak využít funkci Generative Fill v Photoshopu a Premiere Pro pro rozšiřování natočeného záběru',
+    },
+    {
+        id: 2,
+        title: 'Dreamy scéna | Postprodukce kreativního nápadu',
+        videoUrl: 1052801906,
+        preview: 1052801906,
+        description:
+            'V tomto tutoriálu si ukážeme, jak vytvořit filmovou snovou scénu pomocí postprodukce, zaměřenou na práci se světlem, barvami a atmosférou pro magický efekt.',
     },
 ]
