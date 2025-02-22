6e383b3fabf6dcf5528395e854b588b383bbb5aa
Nepokornyi <vadymkin@gmail.com>
2025-02-22 19:06:00 +0100

    feat: add onClick handler for Box component

--- nextjs_rubinprod/src/components/Box/Box.tsx
+++ nextjs_rubinprod/src/components/Box/Box.tsx
@@ -3,8 +3,13 @@ import React, { ReactNode } from 'react'
 type BoxProps = {
     children: ReactNode
     className?: string
+    onClick?: () => void
 }
 
-export const Box = ({ children, className = '' }: BoxProps) => {
-    return <div className={`relative ${className}`}>{children}</div>
+export const Box = ({ children, className = '', onClick }: BoxProps) => {
+    return (
+        <div onClick={onClick} className={`relative ${className}`}>
+            {children}
+        </div>
+    )
 }
