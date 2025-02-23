a9f4b14d55bded74572a5aa093bdcbf1a45a5658
Nepokornyi <vadymkin@gmail.com>
2025-02-23 16:34:59 +0100

    feat: update videos

--- nextjs_rubinprod/src/config/freeVideos.ts
+++ nextjs_rubinprod/src/config/freeVideos.ts
@@ -17,22 +17,6 @@ export const freeVideoConfig = [
     },
     {
         id: 3,
-        title: 'Profese Video Editora za 2 hodiny',
-        videoUrl: 1042286031,
-        preview: 1042286031,
-        description:
-            'Toto video vás provede klíčovými funkcemi Adobe Premiere Pro 2025 – od organizace projektu a importu záběrů až po finální export. Žádná omáčka, jenom praktické znalosti z reálného světa střihu videí.',
-    },
-    {
-        id: 4,
-        title: 'Telefon nebo Kamera: Jak vybrat správný nástroj pro tvorbu obsahu?',
-        videoUrl: 1045037548,
-        preview: 1045037548,
-        description:
-            'Igor Glinka, student filmové školy ze zkušenostmi vedení produkčních týmů, shrnuje výhody a nevýhody natáčení na telefon i kameru. Zjistíte, který nástroj se hodí pro vaše potřeby, a získáte praktické tipy pro tvorbu obsahu.',
-    },
-    {
-        id: 5,
         title: 'Produkční den sportovního spotu | Tenis',
         videoUrl: 1051244389,
         preview: 1051244389,
--- nextjs_rubinprod/src/config/paidVideos.ts
+++ nextjs_rubinprod/src/config/paidVideos.ts
@@ -1,6 +1,14 @@
 export const paidVideoConfig = [
     {
         id: 1,
+        title: 'Procreate Dreams návod | Rychlý průvodce pro začátečníky',
+        videoUrl: 1057270628,
+        preview: 1057270628,
+        description:
+            'Ukážeme si základy Procreate Dreams na reálné zakázce pro našeho klienta Footshop. Naučíte se klíčové funkce a tipy pro efektivní tvorbu animací.',
+    },
+    {
+        id: 2,
         title: 'Generative Fill ve videu? Photoshop + PremirePro | Tutorial',
         videoUrl: 1042303511,
         preview: 1042303511,
@@ -8,11 +16,27 @@ export const paidVideoConfig = [
             'V tomto tutoriálu vám ukážeme, jak využít funkci Generative Fill v Photoshopu a Premiere Pro pro rozšiřování natočeného záběru',
     },
     {
-        id: 2,
+        id: 3,
         title: 'Dreamy scéna | Postprodukce kreativního nápadu',
         videoUrl: 1052801906,
         preview: 1052801906,
         description:
             'V tomto tutoriálu si ukážeme, jak vytvořit filmovou snovou scénu pomocí postprodukce, zaměřenou na práci se světlem, barvami a atmosférou pro magický efekt.',
+    },
+    {
+        id: 4,
+        title: 'Profese Video Editora za 2 hodiny',
+        videoUrl: 1042286031,
+        preview: 1042286031,
+        description:
+            'Toto video vás provede klíčovými funkcemi Adobe Premiere Pro 2025 – od organizace projektu a importu záběrů až po finální export. Žádná omáčka, jenom praktické znalosti z reálného světa střihu videí.',
+    },
+    {
+        id: 5,
+        title: 'Telefon nebo Kamera: Jak vybrat správný nástroj pro tvorbu obsahu?',
+        videoUrl: 1045037548,
+        preview: 1045037548,
+        description:
+            'Igor Glinka, student filmové školy ze zkušenostmi vedení produkčních týmů, shrnuje výhody a nevýhody natáčení na telefon i kameru. Zjistíte, který nástroj se hodí pro vaše potřeby, a získáte praktické tipy pro tvorbu obsahu.',
     },
 ]
