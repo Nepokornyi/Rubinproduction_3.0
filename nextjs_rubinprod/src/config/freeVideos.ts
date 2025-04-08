b66a9bb5923b443260a3be8d45d84493734a6666
Nepokornyi <vadymkin@gmail.com>
2025-04-08 15:35:13 +0200

    refactor video config files

--- nextjs_rubinprod/src/config/freeVideos.ts
+++ nextjs_rubinprod/src/config/freeVideos.ts
@@ -1,57 +1,50 @@
 export const freeVideoConfig = [
     {
-        id: 1059498916,
+        id: 7,
         title: 'Kateřina Sýsová - Cesta fotografa: Od první fotky až po vlastní výstavu',
         videoUrl: 1059498916,
-        preview: 1059498916,
         description:
             'Kateryna Sysova sdílí svou cestu od prvních fotografických pokusů až po vlastní výstavu. Jaké byly její začátky, co ji inspirovalo a jak se dostala tam, kde je dnes?',
     },
     {
-        id: 1,
+        id: 6,
         title: 'Mark Bliss ACS - Jak se dostat do Kinematografie?',
         videoUrl: 1042304330,
-        preview: 1042919596,
         description:
             'Rozhovor s držitelem prestižních ocenění a vítězem Cannes Festivalu! Mark Bliss ACS sdílí svůj příběh o cestě do světa kinematografie, inspiraci a zákulisí úspěchu. Dozvíte se, co stojí za jeho kariérou a jaké rady má pro začínající i pokročilé tvůrce',
     },
     {
-        id: 2,
+        id: 5,
         title: 'Tomáš Nygrýn - Creative Director | ShareYourself Agency',
         videoUrl: 1042300569,
-        preview: 1042916606,
         description:
             'Zjistěte, jak vypadá běžný den Creative Directora při spolupráci s klientem. Tomáš Nygrýn sdílí své know-how, tipy na efektivní workflow a náhled do kreativního procesu.',
     },
     {
-        id: 3,
+        id: 4,
         title: 'Produkční den sportovního spotu | Tenis',
         videoUrl: 1051244389,
-        preview: 1051244389,
         description:
             'Zorganizovat natáčení reklamního spotu není jednoduché - nápad, lokace, tým, technika, modelky, struktura a postprodukce. Ale občas není potřeba mít velké budgety, zvlášť v případě, když děláte projekt do vlastního portfolia.',
     },
     {
-        id: 1059455885,
+        id: 3,
         title: 'Jak videotvůrci najít klienty | natáčeni reklamy pro Footshop',
         videoUrl: 1059455885,
-        preview: 1059455885,
         description:
             'V dnešním dílu našich týdenních vlogů se podíváme na kejsku jednoho aktuálního projektu našeho klienta - FootShop. Dozvíš se, jak probíhá proces komerčního natáčení, nahledneš do zákulisí tvorby virálních trendových videí a seznámíš se s profi oboru video/fototvorby.',
     },
     {
-        id: 1059464512,
+        id: 2,
         title: 'Video Clip | Production Day',
         videoUrl: 1059464512,
-        preview: 1059464512,
         description:
             'V tomto dílu se společně podíváme, jak se nám podařilo klukovi z vesnice natočit klip americké kvality! Krok za krokem. Byla to obrovská výzva ale zároveň velká zkušenost.',
     },
     {
-        id: 1059461939,
+        id: 1,
         title: 'FPV Real Estate | Production Day',
         videoUrl: 1059461939,
-        preview: 1059461939,
         description:
             'Jak nestandardní přístupy ve videotvorbě mohou přinést klientovi obrovskou výhodu a nový úhel pohledu na propagaci svých služeb či produktu.',
     },
--- nextjs_rubinprod/src/config/paidVideos.ts
+++ nextjs_rubinprod/src/config/paidVideos.ts
@@ -1,50 +1,56 @@
-export const paidVideoConfig = [
-    {
-        id: 1,
+export const paidVideoConfig = {
+    'procreate-dreams': {
+        id: 6,
         title: 'Procreate Dreams návod | Rychlý průvodce pro začátečníky',
-        videoUrl: 1057270628,
-        preview: 1057270628,
+        videoId: 1057270628,
+        thumbnail:
+            'https://i.vimeocdn.com/video/1985886442-8f583bac8db18986a834761d32a2dc3b36788c89d734816a1c8953666eaf5cc9-d',
         description:
             'Ukážeme si základy Procreate Dreams na reálné zakázce pro našeho klienta Footshop. Naučíte se klíčové funkce a tipy pro efektivní tvorbu animací.',
     },
-    {
-        id: 2,
-        title: 'Generative Fill ve videu? Photoshop + PremirePro | Tutorial',
-        videoUrl: 1042303511,
-        preview: 1042303511,
+    'generative-fill': {
+        id: 5,
+        title: 'Generative Fill ve videu? Photoshop + PremierePro | Tutorial',
+        videoId: 1042303511,
+        thumbnail:
+            'https://i.vimeocdn.com/video/1985884078-8fd56a043cf0d50b03e31f52c9ed9b7eb7a090a498e795ae64924d80f188e022-d',
         description:
             'V tomto tutoriálu vám ukážeme, jak využít funkci Generative Fill v Photoshopu a Premiere Pro pro rozšiřování natočeného záběru',
     },
-    {
-        id: 3,
+    'dreamy-scene': {
+        id: 4,
         title: 'Dreamy scéna | Postprodukce kreativního nápadu',
-        videoUrl: 1052801906,
-        preview: 1052801906,
+        videoId: 1052801906,
+        thumbnail:
+            'https://i.vimeocdn.com/video/1985881536-9b9799a2d624a2a84b55122d5bdac5fc4dc56d8b69cd57e8a0f9d796c48c5af4-d',
         description:
             'V tomto tutoriálu si ukážeme, jak vytvořit filmovou snovou scénu pomocí postprodukce, zaměřenou na práci se světlem, barvami a atmosférou pro magický efekt.',
     },
-    {
-        id: 4,
+    'video-editor': {
+        id: 3,
         title: 'Profese Video Editora za 2 hodiny',
-        videoUrl: 1042286031,
-        preview: 1042286031,
+        videoId: 1042286031,
+        thumbnail:
+            'https://i.vimeocdn.com/video/1985890061-a10cd104bef63b988b53cafe58c9b914ccb7d757936d80c5b9c19ca99225fe26-d',
         description:
             'Toto video vás provede klíčovými funkcemi Adobe Premiere Pro 2025 – od organizace projektu a importu záběrů až po finální export. Žádná omáčka, jenom praktické znalosti z reálného světa střihu videí.',
     },
-    {
-        id: 5,
+    'phone-or-camera': {
+        id: 2,
         title: 'Telefon nebo Kamera: Jak vybrat správný nástroj pro tvorbu obsahu?',
-        videoUrl: 1045037548,
-        preview: 1045037548,
+        videoId: 1045037548,
+        thumbnail:
+            'https://i.vimeocdn.com/video/1985887337-805489ddab0de2a701c0de9423380d331271bc16ce843cfb75636505844a0207-d',
         description:
             'Igor Glinka, student filmové školy ze zkušenostmi vedení produkčních týmů, shrnuje výhody a nevýhody natáčení na telefon i kameru. Zjistíte, který nástroj se hodí pro vaše potřeby, a získáte praktické tipy pro tvorbu obsahu.',
     },
-    {
-        id: 6,
+    'fashion-video': {
+        id: 1,
         title: 'Střih Fashion Videa | After Effects',
-        videoUrl: 1059480744,
-        preview: 1059480744,
+        videoId: 1059480744,
+        thumbnail:
+            'https://i.vimeocdn.com/video/1985879174-1abd5e4da1cff874c6f21f991df75434af2688db0ec57a217bb777c6f07926b3-d',
         description:
             'V tomto podrobném tutoriálu si společně rozebereme postup při střihu fashion videa. Sdělíme tipy na kreativní proces a zapojení programů na vizuální efekty After Effects',
     },
-]
+}
